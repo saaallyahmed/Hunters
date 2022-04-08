5b4d2e493161a69446b38057e162d49ec747a98a
Kalai G <manols20@gmail.com>
2022-04-07 18:14:09 -0700

    final updates

---
+++ Mano/static/js/amcharts.js
@@ -0,0 +1,126 @@
+am5.ready(function() {
+    
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root = am5.Root.new("chartdiv");
+    
+    
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+    
+    
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/
+    var chart = root.container.children.push(am5xy.XYChart.new(root, {
+      panX: true,
+      panY: true,
+      wheelX: "panX",
+      wheelY: "zoomX",
+      pinchZoomX:true
+    }));
+    
+    // Add cursor
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+    var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+    cursor.lineY.set("visible", false);
+    
+    
+    // Create axes
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+    var xRenderer = am5xy.AxisRendererX.new(root, { minGridDistance: 30 });
+    xRenderer.labels.template.setAll({
+      rotation: -90,
+      centerY: am5.p50,
+      centerX: am5.p100,
+      paddingRight: 15
+    });
+    
+    var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+      maxDeviation: 0.3,
+      categoryField: "zodiac",
+      renderer: xRenderer,
+      tooltip: am5.Tooltip.new(root, {})
+    }));
+    
+    var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+      maxDeviation: 0.3,
+      renderer: am5xy.AxisRendererY.new(root, {})
+    }));
+    
+    
+    // Create series
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+    var series = chart.series.push(am5xy.ColumnSeries.new(root, {
+      name: "Series 1",
+      xAxis: xAxis,
+      yAxis: yAxis,
+      valueYField: "value",
+      sequencedInterpolation: true,
+      categoryXField: "zodiac",
+      tooltip: am5.Tooltip.new(root, {
+        labelText:"{valueY}"
+      })
+    }));
+    
+    series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5 });
+    series.columns.template.adapters.add("fill", function(fill, target) {
+      return chart.get("colors").getIndex(series.columns.indexOf(target));
+    });
+    
+    series.columns.template.adapters.add("stroke", function(stroke, target) {
+      return chart.get("colors").getIndex(series.columns.indexOf(target));
+    });
+    
+    
+    // Set data
+    var data = [{
+      zodiac: "Aries",
+      value: 3
+    }, {
+      zodiac: "Taurus",
+      value: 1
+    }, {
+      zodiac: "Gemini",
+      value: 3
+    }, {
+      zodiac: "Cancer",
+      value: 9
+    }, {
+      zodiac: "Leo",
+      value: 11
+    }, {
+      zodiac: "Virgo",
+      value: 13
+    }, {
+      zodiac: "Libra",
+      value: 11
+    }, {
+      zodiac: "Scorpio",
+      value: 11
+    }, {
+      zodiac: "Sagittarius",
+      value: 6
+    }, {
+      zodiac: "Capricorn",
+      value: 4
+    }, {
+      zodiac: "Aquarius",
+      value: 5
+    }, {
+      zodiac: "Pisces",
+      value: 3
+    }];
+    
+    xAxis.data.setAll(data);
+    series.data.setAll(data);
+    
+    
+    // Make stuff animate on load
+    // https://www.amcharts.com/docs/v5/concepts/animations/
+    series.appear(1000);
+    chart.appear(1000, 100);
+    
+    }); // end am5.ready()
